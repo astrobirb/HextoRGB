9dd3eac425e5c80d75652e6904b1d56456765cb1
astrobirb <31900770+astrobirb@users.noreply.github.com>
2017-10-10 13:16:08 -0600

    Final commit

--- ViewController.cs
+++ ViewController.cs
@@ -15,6 +15,26 @@ namespace HextoRGB
         {
             base.ViewDidLoad();
             // Perform any additional setup after loading the view, typically from a nib.
+
+            convertButton.TouchUpInside+= ConvertButton_TouchUpInside;
+        }
+
+        void ConvertButton_TouchUpInside(object sender, EventArgs e)
+        {
+            string hexValue = hexValueTextField.Text;
+            string redHexValue = hexValue.Substring(0, 2);
+            string greenHexValue = hexValue.Substring(2, 2);
+            string blueHexValue = hexValue.Substring(4, 2);
+
+            int redValue = int.Parse(redHexValue, System.Globalization.NumberStyles.HexNumber);
+            int greenValue = int.Parse(greenHexValue, System.Globalization.NumberStyles.HexNumber);
+            int blueValue = int.Parse(blueHexValue, System.Globalization.NumberStyles.HexNumber);
+
+            redValueLabel.Text = redValue.ToString();
+            greenValueLabel.Text = greenValue.ToString();
+            blueValueLabel.Text = blueValue.ToString();
+
+            colorView.BackgroundColor = UIColor.FromRGB(redValue, greenValue, blueValue);
         }
 
         public override void DidReceiveMemoryWarning()
--- ViewController.designer.cs
+++ ViewController.designer.cs
@@ -1,17 +1,73 @@
-﻿//
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
 
 namespace HextoRGB
 {
-    [Register("ViewController")]
+    [Register ("ViewController")]
     partial class ViewController
     {
-        void ReleaseDesignerOutlets()
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel blueValueLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIView colorView { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton convertButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel greenValueLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField hexValueTextField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel redValueLabel { get; set; }
+
+        void ReleaseDesignerOutlets ()
         {
+            if (blueValueLabel != null) {
+                blueValueLabel.Dispose ();
+                blueValueLabel = null;
+            }
+
+            if (colorView != null) {
+                colorView.Dispose ();
+                colorView = null;
+            }
+
+            if (convertButton != null) {
+                convertButton.Dispose ();
+                convertButton = null;
+            }
+
+            if (greenValueLabel != null) {
+                greenValueLabel.Dispose ();
+                greenValueLabel = null;
+            }
+
+            if (hexValueTextField != null) {
+                hexValueTextField.Dispose ();
+                hexValueTextField = null;
+            }
+
+            if (redValueLabel != null) {
+                redValueLabel.Dispose ();
+                redValueLabel = null;
+            }
         }
     }
 }
