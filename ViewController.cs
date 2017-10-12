97dadff04d371b31435719fbdd73fe94f4703dc2
astrobirb <31900770+astrobirb@users.noreply.github.com>
2017-10-12 13:13:52 -0600

    Commented and cleaned

--- ViewController.cs
+++ ViewController.cs
@@ -16,24 +16,33 @@ namespace HextoRGB
             base.ViewDidLoad();
             // Perform any additional setup after loading the view, typically from a nib.
 
+            // "Connects" code to the app
             convertButton.TouchUpInside+= ConvertButton_TouchUpInside;
         }
 
+        // Code(aka method) for app's functions
         void ConvertButton_TouchUpInside(object sender, EventArgs e)
         {
+            // Variable for the user's written input
             string hexValue = hexValueTextField.Text;
+            // Variable for the first two numbers and/or letters
             string redHexValue = hexValue.Substring(0, 2);
+            // Variable for the following two numbers and/or letters
             string greenHexValue = hexValue.Substring(2, 2);
+            // Variable for the last two numbers and/or letters
             string blueHexValue = hexValue.Substring(4, 2);
 
+            // Converts the hex values to their RGB counterparts
             int redValue = int.Parse(redHexValue, System.Globalization.NumberStyles.HexNumber);
             int greenValue = int.Parse(greenHexValue, System.Globalization.NumberStyles.HexNumber);
             int blueValue = int.Parse(blueHexValue, System.Globalization.NumberStyles.HexNumber);
 
+            // Displays the converted RGB values onto their respective text areas in the app
             redValueLabel.Text = redValue.ToString();
             greenValueLabel.Text = greenValue.ToString();
             blueValueLabel.Text = blueValue.ToString();
 
+            // Invisible area that changes colour depending on the colour value found
             colorView.BackgroundColor = UIColor.FromRGB(redValue, greenValue, blueValue);
         }
 
